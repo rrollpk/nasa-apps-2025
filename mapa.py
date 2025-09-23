e64b285847bbb34b82ba726e4620d01e1da5f905
Rodrigo Roldán <rrollpk@gmail.com>
2025-09-23 06:40:31 +0200

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -5,7 +5,7 @@ from streamlit_folium import st_folium
 # Crear mapa centrado en coordenadas
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=5)
 
-
+folium.GeoJson("spain_Municipality_level_3.geojson", name = 'India').add_to(m)
 
     
 # Añadir marcador para ciudad
@@ -20,6 +20,7 @@ folium.CircleMarker(
 
 # Mostrar en Streamlit
 st_folium(m, width=700, height=500)
+
 
 
 
